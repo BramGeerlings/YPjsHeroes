8932f0a9d35f6c68e582dd299629c527551b599e
BramGeerlings <geerlingsb@gmail.com>
2016-06-08 22:21:12 +0200

    US1 Bram Parameter parser gemaakt. Nog niet tevreden. Door naar US2.

--- mortgage-calc-ctrl.js
+++ mortgage-calc-ctrl.js
@@ -11,12 +11,14 @@ document.getElementById("submitButton").addEventListener("click", validateAndSub
 function validateAndSubmit() {   
     var val1 = document.getElementsByName("incomeField")[0].value;
     var val2 = document.getElementsByName("incomeField")[1].value;      
-    
+   
     var http = new XMLHttpRequest();
     var url = 'http://agile-wave-86684.herokuapp.com/max-to-loan';
-    var params = "income1=" + val1 + "&" + "income2=" +val2;
-
+    var params = quickParseParams(val1,val2);
+    
+    //TODO: Betere manier vinden om params op te maken. JSON.stringify achtige manier, maar dan zonder encoding??
     http.open("GET", url+"?"+params,true);
+
     http.onreadystatechange = function()
     {
         if(http.readyState == 4 && http.status == 200) {           
@@ -25,7 +27,16 @@ function validateAndSubmit() {
             document.getElementById('total').innerHTML = 'Voer alleen numerieke karakters in';
         }
     }
+
     http.send(null);
 }
 
+function quickParseParams(){
+    var parameter = "";
+    for(var i = 0; i< arguments.length; i++){
+        i<arguments.length-1? parameter += "income" + (i+1)+ "=" +(arguments[i]+"&"):
+        parameter += "income" + (i+1)+ "=" +arguments[i];
+    }
+    return parameter;
+}
 
