ae4d83079715635dd266f013abd0eca7cdd63568
BramGeerlings <geerlingsb@gmail.com>
2016-06-10 21:59:20 +0200

    US3 US5 Bram verdere opmaak gedaan en rente toegevoegd

--- mortgage-calc-ctrl.js
+++ mortgage-calc-ctrl.js
@@ -1,15 +1,11 @@
-console.clear();
-
 window.onload = function () {
-    init();
-
+    showPage("Calculator");
 }
 
 for (var i = 0; i < document.getElementsByName("incomeField").length; i++) {
     document.getElementsByName("incomeField")[i].addEventListener("change", validateAndSubmit)
 }
 
-//document.getElementById("submitButton").addEventListener("click", validateAndSubmit)
 for (element of document.getElementsByClassName("menu-button")) {
     element.addEventListener("click", showPage);
 }
@@ -17,6 +13,18 @@ for (element of document.getElementsByClassName("menu-button")) {
 var menuButtons = document.getElementsByClassName('menu-button');
 
 function validateAndSubmit() {
+    switch (true) {
+        case !!document.getElementById("page_calculator"):
+            calculateMortgage();
+            break;
+        case !!document.getElementById("page_rente"):
+            calculateInterest();
+            break;
+    }
+
+}
+
+function calculateMortgage() {
     var val1 = document.getElementsByName("incomeField")[0].value;
     var val2 = document.getElementsByName("incomeField")[1].value;
 
@@ -24,13 +32,58 @@ function validateAndSubmit() {
     var url = 'http://agile-wave-86684.herokuapp.com/max-to-loan';
     var params = quickParseParams(val1, val2);
 
-    http.open("GET", url + "?" + params, true);
+    http.open("GET", url + "?" + params, true);   
+  
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            var maxTeLenen = parseInt(JSON.parse(http.responseText).maxToLoan);
+            maxTeLenen = parseFloat(maxTeLenen).formatMoney(2,",");
+            document.getElementById('total').innerHTML = "U kunt maximaal lenen: " + maxTeLenen + " euro";
+        } else if (http.readyState == 4 && http.status != 200) {
+            document.getElementById('total').innerHTML = 'Voer alleen numerieke karakters in';
+        }
+    }
+
+    http.send(null);
+}
+
+
+
+function calculateInterest() {
+    var val = document.getElementsByName("incomeField")[0].value;
+    var interest = ' ';
+    if (document.getElementById('currentRate')) {
+        interest = parseFloat(document.getElementById('currentRate').innerHTML);
+    }
+    interest = interest / 100;
+    var trueInterest = Math.round(((val / 12) * interest) * 100) / 100;
+    trueInterest = parseFloat(trueInterest).formatMoney(2,",");
+    document.getElementById('total').innerHTML = 'Bruto maandlast: ' + trueInterest + ' euro';
+}
+
+///NIET ZELF GESCHREVEN!!!
+//http://www.josscrowcroft.com/2011/code/format-unformat-money-currency-javascript/
+//Alleen aangepast naar europees format.
+Number.prototype.formatMoney = function(places, symbol, thousand, decimal) {
+	places = !isNaN(places = Math.abs(places)) ? places : 2;	
+	thousand = thousand || ".";
+	decimal = decimal || ",";
+	var number = this, 
+	    negative = number < 0 ? "-" : "",
+	    i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+	    j = (j = i.length) > 3 ? j % 3 : 0;
+	return negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+};
+
+function getCurrentInterestRate() {
+    var http = new XMLHttpRequest();
+    var url = 'http://agile-wave-86684.herokuapp.com/current-mortgage-interest-rate';
+
+    http.open("GET", url, true);
 
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
-            document.getElementById('total').innerHTML = JSON.parse(http.responseText).maxToLoan;
-        } else if (http.readyState == 4 && http.status != 200) {
-            document.getElementById('total').innerHTML = 'Voer alleen numerieke karakters in';
+            document.getElementById('currentRate').innerHTML = JSON.parse(http.responseText).currentRate;
         }
     }
 
@@ -44,7 +97,7 @@ function showPage(e) {
         selectedPage = setMenuButtonToActive(e);
 
     for (page of pages) {
-        if (document.getElementById("content")) {          
+        if (document.getElementById("content")) {
             document.getElementById("content").innerHTML = quickParseObjectToString(selectedPage);
         }
     }
@@ -59,7 +112,6 @@ function setMenuButtonToActive(buttonName) {
             menuButton.style.backgroundColor = '#fd6400';
         }
     }
-
     return buttonName;
 }
 
@@ -79,7 +131,8 @@ function quickParseParams() {
     return parameter;
 }
 
-function init() {
-    showPage("Calculator");
+if (document.getElementById('currentRate')) {
+    getCurrentInterestRate();
 }
+
 
